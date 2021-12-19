00922ffbe0c3e8702b3652321a39b04ec0e07407
Ivan Perepelkin <perepelkin2601@gmail.com>
2021-12-20 00:57:34 +0500

    fix Receipts repo

--- src/main/kotlin/repository/Receipts.kt
+++ src/main/kotlin/repository/Receipts.kt
@@ -16,24 +16,24 @@ class Receipts(private val database: Database) : ReceiptRepo {
 
             val amount = fullAmount / to.count()
 
-            to.forEach{
-                userId ->
-                    ReceiptTransactionsTable.insert { data ->
-                        data[ReceiptTransactionsTable.receiptId] = receipt.value
-                        data[ReceiptTransactionsTable.toParticipant] = userId.id
-                        data[ReceiptTransactionsTable.fromParticipant] = from.id
-                        data[ReceiptTransactionsTable.amount] = amount
-                    }
+            to.forEach { userId ->
+                ReceiptTransactionsTable.insert { data ->
+                    data[receiptId] = receipt.value
+                    data[toParticipant] = userId.id
+                    data[fromParticipant] = from.id
+                    data[ReceiptTransactionsTable.amount] = amount
+                }
             }
 
-            Receipt(ReceiptId(receipt.value),from,to,fullAmount)
+            Receipt(ReceiptId(receipt.value), from, to, fullAmount)
         }
     }
 
     override fun removeReceipt(id: ReceiptId): Boolean {
         return transaction(database)
         {
-            val deletedTransactions = ReceiptTransactionsTable.deleteWhere { ReceiptTransactionsTable.receiptId eq id.id}
+            val deletedTransactions =
+                ReceiptTransactionsTable.deleteWhere { ReceiptTransactionsTable.receiptId eq id.id }
             val deleted = ReceiptsTable.deleteWhere { ReceiptsTable.id eq id.id }
 
             deleted > 0 && deletedTransactions > 0
@@ -44,14 +44,15 @@ class Receipts(private val database: Database) : ReceiptRepo {
         return transaction(database)
         {
             val resultQuery =
-                    ReceiptTransactionsTable.innerJoin(ReceiptsTable, {ReceiptTransactionsTable.receiptId},
-                {ReceiptsTable.id}).select {ReceiptTransactionsTable.receiptId eq id.id}//.groupBy { ReceiptTransactionsTable.fromParticipant }
+                ReceiptTransactionsTable.innerJoin(ReceiptsTable, { receiptId },
+                    { ReceiptsTable.id })
+                    .select { ReceiptTransactionsTable.receiptId eq id.id }//.groupBy { ReceiptTransactionsTable.fromParticipant }
 
             Receipt(
-                    id = id,
-                    from =  UserId(resultQuery.map { it[ReceiptTransactionsTable.fromParticipant]}.first()),
-                    to =  resultQuery.map { UserId(it[ReceiptTransactionsTable.toParticipant])} ,
-                    amount = resultQuery.map { it[ReceiptsTable.fullAmount] }.first(),
+                id = id,
+                from = UserId(resultQuery.map { it[ReceiptTransactionsTable.fromParticipant] }.first()),
+                to = resultQuery.map { UserId(it[ReceiptTransactionsTable.toParticipant]) },
+                amount = resultQuery.map { it[ReceiptsTable.fullAmount] }.first(),
             )
         }
     }
@@ -59,19 +60,37 @@ class Receipts(private val database: Database) : ReceiptRepo {
     override fun getReceipts(chatId: Long): List<Receipt> {
         return transaction(database)
         {
-            //val resultQuery =
-                ReceiptTransactionsTable.innerJoin(ReceiptsTable, {ReceiptTransactionsTable.receiptId}, {ReceiptsTable.id})
-                .slice(ReceiptTransactionsTable.receiptId, ReceiptTransactionsTable.fromParticipant, ReceiptTransactionsTable.toParticipant, ReceiptsTable.fullAmount)
-                .select {ReceiptsTable.chatId eq chatId}
-                .groupBy {ReceiptTransactionsTable.receiptId}
-                    .map { x ->
-                        Receipt(
-                            id = ReceiptId(x.key.name.toLong()),
-                            from = x.value.map { z -> UserId(z[ReceiptTransactionsTable.fromParticipant]) }.first(),
-                            to = x.value.map { z -> UserId(z[ReceiptTransactionsTable.toParticipant]) },
-                            amount = x.value.map { z -> z[ReceiptsTable.fullAmount] }.first()
-                        )
-                    }
-            }
+
+            ReceiptTransactionsTable
+
+            ReceiptTransactionsTable
+                .innerJoin(
+                    ReceiptsTable,
+                    { receiptId },
+                    { ReceiptsTable.id }
+                )
+                .slice(
+                    ReceiptTransactionsTable.receiptId,
+                    ReceiptTransactionsTable.fromParticipant,
+                    ReceiptTransactionsTable.toParticipant,
+                    ReceiptsTable.fullAmount
+                )
+                .select { ReceiptsTable.chatId eq chatId }
+                .groupBy { row ->
+                    Triple(
+                        row[ReceiptTransactionsTable.receiptId],
+                        row[ReceiptTransactionsTable.fromParticipant],
+                        row[ReceiptsTable.fullAmount]
+                    )
+                }
+                .map { (triple, rows) ->
+                    Receipt(
+                        id = ReceiptId(triple.first),
+                        from = UserId(triple.second),
+                        to = rows.map { UserId(it[ReceiptTransactionsTable.toParticipant]) },
+                        amount = triple.third
+                    )
+                }
+        }
     }
 }
